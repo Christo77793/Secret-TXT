e657695c72e94157b056ee50b363cd59553fdae2
Christopher <christo77793@gmail.com>
2020-12-21 12:41:48 +0530

    A folder named Secret TXT will be automatically created

--- hide.py
+++ hide.py
@@ -5,6 +5,7 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
 from stegano import lsb  # Stegano Library
 
+
 # Window.size = (850, 550)  # Sets the GUI size to start with
 
 
@@ -20,27 +21,32 @@ class SelectScreen(Screen):  # A class to initialise the screen to choose betwee
 class EncodeScreen(Screen):  # A class to initalise the encode screen
 
     def set_text(self):  # Fn to get file path
+
         try:
             self.ids.encode_image_path.text = EncodeScreen.file_path
         except:
             self.ids.encode_image_path.text = "No image selected"
 
     def encode_txt_img(self):  # Fn to encode our txt to image
+
+        sel_image = self.ids.encode_image_path.text
+        user_text = self.ids.user_text.text
+
+        if (os.path.exists(r"C:\Secret TXT")):
+            pass
+        else:
+            # os.chdir(r"C:")
+            os.makedirs(r"C:\Secret TXT")
+
+        secret_img = r"C:\Secret TXT\encoded_img.png"
+
         try:
-            sel_image = self.ids.encode_image_path.text
-            user_text = self.ids.user_text.text
-            secret_img = r"C:\Secret Text\encoded_img.png"
+            lsb.hide(sel_image, message=user_text).save(secret_img)
 
-            try:
-                lsb.hide(sel_image, message=user_text).save(secret_img)
+            self.ids.encode_image_path.text = ""
+            self.ids.user_text.text = ""
 
-                self.ids.encode_image_path.text = ""
-                self.ids.user_text.text = ""
-
-                self.ids.encode_result.text = "Encoding was successful"
-            except:
-
-                self.ids.encode_result.text = "Encoding has failed"
+            self.ids.encode_result.text = "Encoding was successful"
         except:
 
             self.ids.encode_result.text = "Encoding has failed"
@@ -54,6 +60,7 @@ class EncodeScreen(Screen):  # A class to initalise the encode screen
 class SelectFileEncode(Screen):  # A class to select a file
 
     def get_source_encode(self, filename):
+
         try:
             self.ids.image.source = filename[0]
             EncodeScreen.file_path = filename[0]
@@ -71,16 +78,18 @@ class DecodeScreen(Screen):  # A class to initalise the decode screen
             self.ids.decode_image_path.text = "No image selected"
 
     def decode_img_txt(self):  # Fn to decode our txt from our image
-        try:
-            sel_image = self.ids.decode_image_path.text
-            hidden_text = lsb.reveal(sel_image)
-            try:
-                self.ids.text_decoded.text = hidden_text
 
-                self.ids.decode_result.text = "Decoding was successful"
-            except:
-                self.ids.decode_result.text = "Decoding has failed"
+        sel_image = self.ids.decode_image_path.text
+
+        try:
+            hidden_text = lsb.reveal(sel_image)
+            print(f"Test_is! {hidden_text}")
+
+            self.ids.text_decoded.text = hidden_text
+            self.ids.decode_result.text = "Decoding was successful"
+
         except:
+            self.ids.text_decoded.text = ""
             self.ids.decode_result.text = "Decoding has failed"
 
     def clear_text(self):  # Fn to clear screen on leaving the screen
